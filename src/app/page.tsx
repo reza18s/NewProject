5a21e0b2998b38088f27ef99bee9f84833415946
reza18s <swary2021@gmail.com>
2024-09-19 17:33:20 +0330

    fix search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,21 @@
 import BuyResidentialsPage from "@/components/home/BuyResidentialsPage";
 import { db } from "@/lib/db";
-import { Profiles } from "@prisma/client";
 async function BuyResidentials({
   searchParams,
 }: {
   searchParams: { category?: string; search?: string };
 }) {
-  let data: Profiles[] = [];
-  if (searchParams.category && searchParams.search) {
-    data = await db.profiles.findMany({
-      where: {
-        title: { contains: searchParams.search },
-        category: searchParams.category,
-      },
+  const where = {};
+  if (searchParams) {
+    Object.keys(searchParams).map((key) => {
+      if (key == "search") {
+        where["title"] = { contains: searchParams[key] };
+      } else {
+        where[key] = searchParams[key];
+      }
     });
-  } else if (searchParams.category) {
-    data = await db.profiles.findMany({
-      where: { category: searchParams.category },
-    });
-  } else if (searchParams.search) {
-    data = await db.profiles.findMany({
-      where: { title: { contains: searchParams.search } },
-    });
-  } else {
-    data = await db.profiles.findMany({});
   }
+  const data = await db.profiles.findMany({ where: where });
 
   if (!data) {
     return <h3>مشکلی پیش آمده است</h3>;

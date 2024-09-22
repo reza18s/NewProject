315a367fa3e71e500200599c8c6e4b00aeb84de4
reza18s <swary2021@gmail.com>
2024-09-22 21:08:23 +0330

    add profile page

--- frontend/src/app/profile/[id]/page.tsx
+++ frontend/src/app/profile/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const PropertySale = () => {
   return (
-    <div className="flex bg-gray-100 p-5 pt-10">
+    <div className="flex bg-gray-100 p-5 pt-10 text-center">
       <div className="m-4 flex w-3/5 flex-col px-20">
         <h1 className="m-2 mx-auto h-20 w-3/5 bg-blue-900 text-center text-5xl font-bold text-white">
           رهن و اجاره مسکونی
@@ -31,14 +31,44 @@ const PropertySale = () => {
             تحویل:تخلیه
           </div>
         </div>
+        <div className="mt-5 flex h-32 justify-between">
+          <div className="mt-auto">
+            <h2 className="h-full text-3xl font-semibold">
+              <span className="text-blue-900"> املاک دایان</span>/اقای
+              شهبازی(100اگهی)
+            </h2>
+          </div>
+          <div className="mb-[-80px] h-full w-32 shrink-0 overflow-hidden rounded-[50%] md:mb-[-90px]">
+            <Image
+              src={"/image/1.jpeg"}
+              alt="Profiles"
+              className="size-full object-cover"
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
       </div>
-      <Image
-        src={"/image/1.jpeg"}
-        alt=""
-        className="h-[90%] w-2/5"
-        width={1000}
-        height={1000}
-      ></Image>
+      <div className="h-[90%] w-2/5">
+        <div className="absolute flex w-2/5 flex-row justify-end pl-20 pt-5 text-center">
+          <div className="ml-5 h-12 w-32 bg-gray-300 text-2xl font-bold text-blue-800">
+            قابل تبدیل
+          </div>
+          <div className="ml-5 h-12 w-32 bg-gray-300 text-2xl font-bold text-blue-800">
+            سه ساعت پیش
+          </div>
+          <div className="h-12 w-32 bg-gray-300 text-2xl font-bold text-blue-800">
+            بازدید:40نفر
+          </div>
+        </div>
+        <Image
+          src={"/image/1.jpeg"}
+          alt=""
+          className="size-full"
+          width={1000}
+          height={1000}
+        ></Image>
+      </div>
     </div>
   );
 };

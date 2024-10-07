118e19e0819b27737e50347a1f523ad1eb489ead
reza18s <swary2021@gmail.com>
2024-10-07 19:13:14 +0330

    fix

--- backend/src/utils/catchAsync.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const catchAsync = (
-  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
-) => {
-  return (req: Request, res: Response, next: NextFunction) =>
-    fn(req, res, next).catch(next);
-};

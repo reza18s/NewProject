3af820141666d362b127209a398f1a9e965a01a3
reza18s <swary2021@gmail.com>
2024-10-08 14:01:28 +0330

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,37 +18,30 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <body className={cn("min-h-screen bg-background font-sans antialiased")}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          disableTransitionOnChange
-          enableSystem
-        >
-          <Layout>
-            <ModalProvider></ModalProvider>
-            {children}
-          </Layout>
-          <Toaster
-            position="top-center"
-            gutter={12}
-            containerStyle={{ margin: "8px" }}
-            toastOptions={{
-              success: {
-                duration: 3000,
-              },
-              error: {
-                duration: 5000,
-              },
-              style: {
-                fontSize: "16px",
-                maxWidth: "500px",
-                padding: "16px 24px",
-                backgroundColor: "var(--color-grey-0)",
-                color: "var(--color-grey-700)",
-              },
-            }}
-          />
-        </ThemeProvider>
+        <Layout>
+          <ModalProvider></ModalProvider>
+          {children}
+        </Layout>
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
       </body>
     </html>
   );
